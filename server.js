c0613a62bc5ad1a4ab4aca17f0691e50565290c8
dKurtalic <dkurtalic1@etf.unsa.ba>
2023-07-12 00:00:28 +0200

    server changes

--- server.js
+++ server.js
@@ -12,12 +12,13 @@ app.use(express.json());
 app.use(cors())
 app.use('/', routes);
 
-mongoose.connect(process.env.MONGO_URI, { useUnifiedTopology: true }).then(() => {
+await mongoose.connect(process.env.MONGO_URI, { useUnifiedTopology: true }).then(() => {
     app.listen(PORT => {
         console.log("Connected to database and app successfully running on port 4000")
     })
 }).catch((error) => {
     console.log(error)
 })
+
 
 
