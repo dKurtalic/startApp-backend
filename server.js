eff0126eec38cc898505597b770e112463d132ce
dKurtalic <dkurtalic1@etf.unsa.ba>
2023-07-11 23:53:08 +0200

    minor fixes

--- server.js
+++ server.js
@@ -1,17 +1,19 @@
 const express = require('express')
 const routes = require('./routes')
 const mongoose = require('mongoose')
+const cors = require('cors')
 const path = require('path')
 require('dotenv').config()
-
+const PORT = process.env.PORT || 4000
 
 const app = express()
 
 app.use(express.json());
+app.use(cors())
 app.use('/', routes);
 
 mongoose.connect(process.env.MONGO_URI, { useUnifiedTopology: true }).then(() => {
-    app.listen(process.env.PORT, () => {
+    app.listen(PORT => {
         console.log("Connected to database and app successfully running on port 4000")
     })
 }).catch((error) => {
